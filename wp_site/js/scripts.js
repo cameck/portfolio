72708e93d3322fcda92dbb7dc3e87d9330a5184c
Cameron Eckelberry <cameck@gmail.com>
2016-06-17 13:08:51 -0400

    Finished Adding scrolling and hiding jQuery effects upon sending a form

--- wp_site/js/scripts.js
+++ wp_site/js/scripts.js
@@ -1,21 +1,46 @@
 jQuery(document).ready(function(){
-// Add smooth scrolling to all links
-jQuery(".anchor-link").on('click', function(event) {
+  // Add smooth scrolling to all links
+  jQuery(".anchor-link").on('click', function(event) {
 
-// Prevent default anchor click behavior
-event.preventDefault();
+    // Prevent default anchor click behavior
+    event.preventDefault();
 
-// Store hash
-var hash = this.hash;
+    // Store hash
+    var hash = this.hash;
 
-// Using jQuery's animate() method to add smooth page scroll
-// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-jQuery('html, body').animate({
-  scrollTop: jQuery(hash).position().top
-}, 800, function(){
+    // Using jQuery's animate() method to add smooth page scroll
+    jQuery('html, body').animate({
+      scrollTop: jQuery(hash).position().top
+    }, 800, function(){
 
-  // Add hash (#) to URL when done scrolling (default click behavior)
-  window.location.hash = hash;
-});
-});
+    // Add hash (#) to URL when done scrolling (default click behavior)
+    window.location.hash = hash;
+    });
+  });
+  // Custom Functionality to hide & show form when Response Message Appears
+  function scrollToContact() {
+    var topLocation =  $('#contact').position().top;
+    $('html,body').animate( { scrollTop: topLocation} , 1000);
+  }
+
+  $.hideForm = function() {
+    jQuery('#ninja_forms_form_5').hide();
+    jQuery('#contact-header h3').hide();
+    scrollToContact();
+  }
+  $.showForm = function() {
+    jQuery('#ninja_forms_form_5').show();
+    jQuery('#contact-header h3').show();
+  }
+
+  var toggleForm = function() {
+
+    if ( document.getElementsByClassName('ninja-forms-success-msg').length ) {
+      $.hideForm();
+    } else {
+      $.showForm();
+    }
+  };
+
+  toggleForm()
 });
